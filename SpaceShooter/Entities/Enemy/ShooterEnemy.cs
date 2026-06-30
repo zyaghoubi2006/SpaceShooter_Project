aa7387bfbb77053eb6e6145c2b8189cceefbc9dd
Zeinab Yaghoubi <z.yaghoubi69@gmail.com>
2026-06-30 18:41:44 +0330

    shooter enemy imlement and drawing a sample of it

--- SpaceShooter/Entities/Enemy/ShooterEnemy.cs
+++ SpaceShooter/Entities/Enemy/ShooterEnemy.cs
@@ -14,5 +14,33 @@ namespace SpaceShooter.Entities.Enemy
         private float fireRate = 1.5f;
         private List<Bullet> enemyBullets;
 
+
+        public ShooterEnemy(int x, int y, Rectangle gameBounds, List<Bullet> enemyBullets) : base(x, y, 45, 45, 80f, 30, 200, gameBounds)
+        {
+            this.enemyBullets = enemyBullets;
+        }
+
+        public override void Update(float deltaTime)
+        {
+            shootTimer += deltaTime;
+            if (shootTimer >= fireRate)
+            {
+
+                shootTimer = 0;
+
+                enemyBullets.Add(new Bullet(X + Width / 2 - 3,Y + Height,6,15,0,250,false));
+
+            }
+
+            Y += Speed * deltaTime;
+
+            if (Y > Bounds.Height) IsAlive = false;
+        }
+
+        public override void Draw(Graphics g)
+        {
+            
+            g.FillRectangle(Brushes.BlueViolet, Bounds);
+        }
     }
 }
