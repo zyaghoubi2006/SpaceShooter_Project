d6dea397b71ec3ef8d6ff3ac554ee571987a46e8
setayesh jahedi <setayeshj76@gmail.com>
2026-07-06 13:07:03 +0330

    modifying program.cs based on the new changes

--- SpaceShooter/Program.cs
+++ SpaceShooter/Program.cs
@@ -1,16 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using SpaceShooter.Forms;
 
-namespace SpaceShooter.Forms
+namespace SpaceShooter
 {
     static class Program
     {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
         [STAThread]
         static void Main()
         {
