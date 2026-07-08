2ff80e28164ee0275f097ddbc468813f6cf1c69a
setayesh jahedi <setayeshj76@gmail.com>
2026-07-09 02:31:21 +0330

    adding the player data class since we need players totalcoins, extralives owned and maxhealth for the player right now, this fields are going to be used in the process of buying items

---
+++ SpaceShooter/Models/PlayerData.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpaceShooter.Models
+{
+    public class PlayerData
+    {
+        public int Id { get; set; }
+        public int TotalCoins { get; set; }
+        public int HighScore { get; set; }
+        public int ExtraLivesOwned { get; set; }
+        public int MaxHealth { get; set; }
+    }
+}
